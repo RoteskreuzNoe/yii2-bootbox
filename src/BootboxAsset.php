607a647ee2a68c1163831067bcdaa500529fd57c
rklvnoe <administrator@n.roteskreuz.at>
2018-11-05 10:07:16 +0100

    init

---
+++ src/BootboxAsset.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: rklvnoe
+ * Date: 2015-03-31
+ * Time: 15:11.
+ */
+
+namespace rklandesverband\bootbox;
+
+use yii\web\AssetBundle;
+
+class BootboxAsset extends AssetBundle
+{
+    public $sourcePath = '@npm/bootbox';
+    public $js = [
+        'bootbox.min.js',
+    ];
+    public $publishOptions = [
+        'only' => [
+            '/bootbox.js',
+            '/bootbox.min.js',
+        ],
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
